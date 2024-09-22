f6613e4ea7ba37f53605ec6ec4925d840db842f2
Daniil Demidko <demidko@farpost.com>
2024-09-23 05:45:43 +0600

    Some fixes

--- src/main/kotlin/com/github/demidko/glock/ApplicationFactory.kt
+++ src/main/kotlin/com/github/demidko/glock/ApplicationFactory.kt
@@ -3,6 +3,7 @@ package com.github.demidko.glock
 import com.github.kotlintelegrambot.entities.ChatId.Companion.fromId
 import com.github.kotlintelegrambot.entities.ChatPermissions
 import com.sksamuel.hoplite.ConfigLoaderBuilder
+import com.sksamuel.hoplite.ExperimentalHoplite
 import com.sksamuel.hoplite.addEnvironmentSource
 import java.time.Duration
 import java.time.Duration.ofMinutes
@@ -18,15 +19,17 @@ open class ApplicationFactory {
     val restrictionsDuration: Duration = ofMinutes(5)
   )
 
+  @OptIn(ExperimentalHoplite::class)
   open val config by lazy {
     ConfigLoaderBuilder.default()
       .addEnvironmentSource()
+      .withExplicitSealedTypes()
       .build()
       .loadConfigOrThrow<Config>()
   }
 
   init {
-    val duration = config.restrictionsDuration.toKotlinDuration().toString()
+    val duration = config.restrictionsDuration.toKotlinDuration()
     println("Restrictions duration: $duration")
   }
 
